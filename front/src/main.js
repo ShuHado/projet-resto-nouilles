461e21795aeaec98ce99fbf8eb161139054f252f
Cyril Arrachequesne <cyrilou564@gmail.com>
2023-03-21 17:25:29 +0100

    ➕ add vuetify

--- front/src/main.js
+++ front/src/main.js
@@ -1,14 +1,18 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "@/App.vue";
+import router from "@/router";
 
-import './assets/main.css'
+import "@/assets/main.css";
 
-const app = createApp(App)
+import vuetify from "@/plugins/vuetify";
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(vuetify);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
