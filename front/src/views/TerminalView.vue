612b5f61a84ebb118801adba68b1252b530b8fd2
Cyril Arrachequesne <cyrilou564@gmail.com>
2023-03-24 15:52:59 +0100

    🔨 add visual for fetch products

--- front/src/views/TerminalView.vue
+++ front/src/views/TerminalView.vue
@@ -1,3 +1,27 @@
+<script setup>
+import { getAllProducts } from "@/services/products.js";
+import { useStore } from "@/stores";
+</script>
+
 <template>
-	<h1>This is HomePage</h1>
+	<div v-for="(product, idx) in products" :key="idx">
+		<h1>{{ product.name }}</h1>
+		<h2>{{ product.price }} €</h2>
+		<img :src="product.img" alt="" />
+		<p>{{ product.categorie.name }}</p>
+	</div>
 </template>
+
+<script>
+export default {
+	data() {
+		return {
+			products: [],
+		};
+	},
+	async mounted() {
+		const token = useStore().getToken;
+		this.products = await getAllProducts(token);
+	},
+};
+</script>
