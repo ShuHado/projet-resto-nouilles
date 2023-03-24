6b3fd81b0422e6d939365b20f0a7bc01625c1a0c
Cyril Arrachequesne <cyrilou564@gmail.com>
2023-03-24 15:56:59 +0100

    🔨 add getAllProducts funtion

--- front/src/services/products.js
+++ front/src/services/products.js
@@ -5,3 +5,18 @@ const instance = axios.create({
 	baseURL:
 		"https://noco-db-production-74ef.up.railway.app/api/v1/db/data/v1/restau-nouilles",
 });
+
+export async function getAllProducts(token) {
+	try {
+		const response = await instance.get("/products", {
+			headers: {
+				"xc-auth": token,
+			},
+		});
+		console.log(response.data.list);
+
+		return response.data.list;
+	} catch (error) {
+		return false;
+	}
+}
