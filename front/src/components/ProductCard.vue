a7c4101a7824522e2bd8df018ef1e72cfec64faf
Cyril Arrachequesne <cyrilou564@gmail.com>
2023-03-25 16:08:30 +0100

    🔨 add card template + card data and card props

---
+++ front/src/components/ProductCard.vue
@@ -0,0 +1,34 @@
+<template>
+	<v-card class="mx-auto" max-width="344">
+		<v-img :src="product.img" height="200px" cover></v-img>
+
+		<v-card-title>{{ product.name }}</v-card-title>
+
+		<v-card-subtitle>{{ product.price }} €</v-card-subtitle>
+
+		<input type="number" v-model="quantity" />
+
+		<v-card-actions>
+			<v-btn
+				color="orange-lighten-2"
+				variant="text"
+				@click="sendProductInfos(product.name, quantity)"
+			>
+				Commander
+			</v-btn>
+		</v-card-actions>
+	</v-card>
+</template>
+
+<script>
+export default {
+	props: {
+		product: Object,
+	},
+	data() {
+		return {
+			quantity: 1,
+		};
+	},
+};
+</script>
