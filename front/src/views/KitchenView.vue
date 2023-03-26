d899d6a5d61005453608056d14b09209f31535b9
Cyril Arrachequesne <cyrilou564@gmail.com>
2023-03-26 18:14:32 +0200

    🔨 create orders table with datas

--- front/src/views/KitchenView.vue
+++ front/src/views/KitchenView.vue
@@ -1,3 +1,41 @@
+<script setup>
+import { getAllOrders } from "@/services/orders.js";
+</script>
+
 <template>
-	<h1>This is an about page</h1>
+	<v-table>
+		<thead>
+			<tr>
+				<th class="text-left">N° de commande</th>
+				<th class="text-center">Status</th>
+				<th class="text-center">Actions</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr v-for="(order, idx) in orders" :key="idx">
+				<td>{{ order.Id }}</td>
+				<td class="text-center">
+					{{ order.processed ? "Complété" : "En cours" }}
+				</td>
+				<td class="text-center">
+					<v-btn color="orange-lighten-2" class="mx-2">Details</v-btn>
+					<v-btn color="orange-lighten-2" class="mx-2">Valider</v-btn>
+				</td>
+			</tr>
+		</tbody>
+	</v-table>
 </template>
+
+<script>
+export default {
+	data() {
+		return {
+			orders: [],
+		};
+	},
+	async mounted() {
+		let orders = await getAllOrders();
+		this.orders = orders;
+	},
+};
+</script>
