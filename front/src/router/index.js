b4149e84b70cf2e5caa81c2c4c7ae864bc15fcf2
Cyril Arrachequesne <cyrilou564@gmail.com>
2023-03-26 18:38:49 +0200

    🔨 create order details route

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -4,6 +4,7 @@ import LoginView from "@/views/LoginView.vue";
 import TerminalView from "@/views/TerminalView.vue";
 import KitchenView from "@/views/KitchenView.vue";
 import CartView from "@/views/CartView.vue";
+import OrderDetailsView from "@/views/OrderDetailsView.vue";
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,6 +29,11 @@ const router = createRouter({
 			meta: {
 				requiresAuth: true,
 			},
+		},
+		{
+			path: "/kitchen/:id",
+			name: "order details view",
+			component: OrderDetailsView,
 		},
 		{
 			path: "/cart",
