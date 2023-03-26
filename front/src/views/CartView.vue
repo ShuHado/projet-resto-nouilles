8ca1acc4357e311cd9a86be07b23f197b8228c4f
Cyril Arrachequesne <cyrilou564@gmail.com>
2023-03-26 14:52:56 +0200

    🔨 add dialog if user want to remove product

--- front/src/views/CartView.vue
+++ front/src/views/CartView.vue
@@ -23,6 +23,37 @@ import { useStore } from "@/stores";
 	<v-btn color="orange-lighten-2" @click="validateCommande"
 		>Valider la commande</v-btn
 	>
+
+	<v-row justify="center">
+		<v-dialog v-model="dialog" persistent width="auto">
+			<v-card>
+				<v-card-title class="text-h5">
+					Supprimer ce produit de votre commande ?
+				</v-card-title>
+				<v-card-text>
+					Êtes-vous sûr de vouloir supprimer
+					{{ commande[remove].product }} de votre commande ?
+				</v-card-text>
+				<v-card-actions>
+					<v-spacer></v-spacer>
+					<v-btn
+						color="green-darken-1"
+						variant="text"
+						@click="dialog = false"
+					>
+						Non
+					</v-btn>
+					<v-btn
+						color="green-darken-1"
+						variant="text"
+						@click="dialog = false"
+					>
+						Oui
+					</v-btn>
+				</v-card-actions>
+			</v-card>
+		</v-dialog>
+	</v-row>
 </template>
 
 <script>
@@ -30,6 +61,8 @@ export default {
 	data() {
 		return {
 			commande: [],
+			dialog: false,
+			remove: null,
 		};
 	},
 	async created() {
@@ -39,6 +72,9 @@ export default {
 		removeProduct(idx) {
 			if (this.commande[idx].quantity > 1) {
 				this.commande[idx].quantity--;
+			} else {
+				this.dialog = true;
+				this.remove = idx;
 			}
 		},
 		addProduct(idx) {
