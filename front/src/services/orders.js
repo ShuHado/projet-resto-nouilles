fc95b5c21e24e5f761ffbc337b947b1e1531d4ca
Cyril Arrachequesne <cyrilou564@gmail.com>
2023-03-26 15:11:26 +0200

    🔨 create orders file for all request for orders

---
+++ front/src/services/orders.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+import { useStore } from "@/stores";
+
+const instance = axios.create({
+	baseURL:
+		"https://noco-db-production-74ef.up.railway.app/api/v1/db/data/v1/restau-nouilles",
+	headers: {
+		"xc-auth": useStore().getToken,
+	},
+});
