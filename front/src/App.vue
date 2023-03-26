93fc9c847f763c88b2ed61d6ae8c82b5012aa4a2
Cyril Arrachequesne <cyrilou564@gmail.com>
2023-03-26 22:47:57 +0200

    💄 add my logo on the site

--- front/src/App.vue
+++ front/src/App.vue
@@ -3,5 +3,14 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
+	<div class="text-center">
+		<img id="my-logo" src="./assets/logo.png" alt="mon logo" />
+	</div>
 	<RouterView />
 </template>
+
+<style scoped>
+#my-logo {
+	width: 10%;
+}
+</style>
