d00be560e002117b700d30771c03f2ec6a075e32
Cyril Arrachequesne <cyrilou564@gmail.com>
2023-03-26 14:31:24 +0200

    💄 create Cart template

---
+++ front/src/views/CartView.vue
@@ -0,0 +1,22 @@
+<template>
+	<ul>
+		<li v-for="(product, idx) in commande" :key="idx">
+			<p>{{ product.product }}</p>
+			<p>{{ product.quantity }}</p>
+			<v-btn
+				color="orange-lighten-2"
+				icon="mdi-minus"
+				@click="removeProduct(idx)"
+			/>
+			<v-btn
+				color="orange-lighten-2"
+				icon="mdi-plus"
+				@click="addProduct(idx)"
+			/>
+		</li>
+	</ul>
+
+	<v-btn color="orange-lighten-2" @click="validateCommande">Valider la commande</v-btn>
+</template>
+
+
