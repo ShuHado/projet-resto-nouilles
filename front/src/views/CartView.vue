8d285736464e253711cd5b9f74360746ac946409
Cyril Arrachequesne <cyrilou564@gmail.com>
2023-03-26 14:32:01 +0200

    🔨 add datas for display of products

--- front/src/views/CartView.vue
+++ front/src/views/CartView.vue
@@ -1,3 +1,7 @@
+<script setup>
+import { useStore } from "@/stores";
+</script>
+
 <template>
 	<ul>
 		<li v-for="(product, idx) in commande" :key="idx">
@@ -16,7 +20,20 @@
 		</li>
 	</ul>
 
-	<v-btn color="orange-lighten-2" @click="validateCommande">Valider la commande</v-btn>
+	<v-btn color="orange-lighten-2" @click="validateCommande"
+		>Valider la commande</v-btn
+	>
 </template>
 
-
+<script>
+export default {
+	data() {
+		return {
+			commande: [],
+		};
+	},
+	async created() {
+		this.commande = useStore().getCommande;
+	},
+};
+</script>
