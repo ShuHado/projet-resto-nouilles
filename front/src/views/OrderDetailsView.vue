7cee9cb113292b9647dff93a7f32308e23465e1e
Cyril Arrachequesne <cyrilou564@gmail.com>
2023-03-26 19:14:45 +0200

    💄 create table view

---
+++ front/src/views/OrderDetailsView.vue
@@ -0,0 +1,18 @@
+<template>
+	<v-table>
+		<thead>
+			<tr>
+				<th></th>
+				<th class="text-left">Produit</th>
+				<th class="text-center">Quantité</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr v-for="(product_object, idx) in order.details" :key="idx">
+				<td>{{ idx }}</td>
+				<td class="text-center">{{ product_object.product }}</td>
+				<td class="text-center">{{ product_object.quantity }}</td>
+			</tr>
+		</tbody>
+	</v-table>
+</template>
